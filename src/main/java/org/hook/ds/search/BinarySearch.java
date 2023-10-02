bc7df924ec19b4583e5fbde4cece93277eca24f1
AskarSap <sapasask@gmail.com>
2023-10-02 13:40:35 +0600

    Search Insert position in sorted array

--- src/main/java/org/hook/ds/search/BinarySearch.java
+++ src/main/java/org/hook/ds/search/BinarySearch.java
@@ -5,13 +5,14 @@ public class BinarySearch {
     BinarySearch bs = new BinarySearch();
     int[] nums = {1, 10, 20, 47, 59, 65, 75, 88, 99};
     System.out.println(bs.binarySearch(nums, 65));
+    System.out.println(bs.searchInsert(nums, 66));
   }
 
   private int binarySearch(int[] nums, int key) {
     int low = 0;
     int high = nums.length - 1;
     while (low <= high) {
-      int mid = (low + high) / 2;
+      int mid = low + (high - low) / 2;
       if (nums[mid] == key) return mid;
       if (key < nums[mid]) {
         high = mid - 1;
@@ -20,5 +21,20 @@ public class BinarySearch {
       }
     }
     return -1;
+  }
+
+  public int searchInsert(int[] arr, int target) {
+    int low = 0;
+    int high = arr.length - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] == target) return mid;
+      if (target < arr[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return low;
   }
 }
