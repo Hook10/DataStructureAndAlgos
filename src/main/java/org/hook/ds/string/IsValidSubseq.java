d0ac9f519dc668ea4cb1b3c04aaab86e58b85817
AskarSap <sapasask@gmail.com>
2023-10-10 19:23:48 +0600

    Is valid subsequence

---
+++ src/main/java/org/hook/ds/string/IsValidSubseq.java
@@ -0,0 +1,19 @@
+package org.hook.ds.string;
+
+public class IsValidSubseq {
+  public static void main(String[] args) {
+    System.out.println(isSubsequence("abcde", "ace"));
+  }
+
+  public static boolean isSubsequence(String str, String seq) {
+    int i = 0;
+    int j = 0;
+    while (i < str.length() && j < seq.length()) {
+      if (str.charAt(i) == seq.charAt(j)) {
+        j++;
+      }
+      i++;
+    }
+    return j == seq.length();
+  }
+}
