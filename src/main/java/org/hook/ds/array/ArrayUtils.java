e94ef8fe9fffb02c39327653acd84acb26b58901
AskarSap <sapasask@gmail.com>
2023-10-11 19:12:49 +0600

    ArrayUtils

--- src/main/java/org/hook/ds/array/ArrayUtils.java
+++ src/main/java/org/hook/ds/array/ArrayUtils.java
@@ -1,19 +1,20 @@
-package org.hook.ds.integer;
+package org.hook.ds.array;
 
-public class Remove {
+public class ArrayUtils {
   public static void main(String[] args) {
-    int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
-    System.out.println(removeElement(nums, 2));
+
   }
 
   public static int removeElement(int[] nums, int val) {
     int i = 0;
     for (int j = 0; j < nums.length; j++) {
-      if (nums[j] != val){
+      if (nums[j] != val) {
         nums[i] = nums[j];
         i++;
       }
     }
     return i;
   }
+
+
 }
