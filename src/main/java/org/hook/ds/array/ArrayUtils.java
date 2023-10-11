c2664e69f7be7770ebb8a2a4ee6de36550d28044
AskarSap <sapasask@gmail.com>
2023-10-11 19:14:18 +0600

    Remove duplicates

--- src/main/java/org/hook/ds/array/ArrayUtils.java
+++ src/main/java/org/hook/ds/array/ArrayUtils.java
@@ -5,6 +5,17 @@ public class ArrayUtils {
 
   }
 
+  public static int removeDuplicates(int[] nums) {
+    int i = 1;
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[j - 1] != nums[j]) {
+        nums[i] = nums[j];
+        i++;
+      }
+    }
+    return i;
+  }
+
   public static int removeElement(int[] nums, int val) {
     int i = 0;
     for (int j = 0; j < nums.length; j++) {
