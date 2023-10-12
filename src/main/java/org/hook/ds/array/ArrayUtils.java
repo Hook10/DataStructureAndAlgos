3f1f510243a48787ecdc9792bae3fb45344a7faa
AskarSap <sapasask@gmail.com>
2023-10-12 15:27:21 +0600

    find Product

--- src/main/java/org/hook/ds/array/ArrayUtils.java
+++ src/main/java/org/hook/ds/array/ArrayUtils.java
@@ -1,8 +1,11 @@
 package org.hook.ds.array;
 
+import java.util.Arrays;
+
 public class ArrayUtils {
   public static void main(String[] args) {
 
+    Arrays.stream(findProduct(new int[]{1, 2, 3, 4})).forEach(e -> System.out.print(e + " "));
   }
 
   public static int removeDuplicates(int[] nums) {
@@ -25,6 +28,41 @@ public class ArrayUtils {
       }
     }
     return i;
+  }
+
+  public static void threeSum(int[] arr, int target) {
+    Arrays.sort(arr);
+    for (int i = 0; i < arr.length - 2; i++) {
+      int j = i + 1;
+      int k = arr.length - 1;
+      while (j < k) {
+        int sum = arr[i] + arr[j] + arr[k];
+        if (sum == target) {
+          System.out.println(arr[i] + "," + arr[j] + "," + arr[k]);
+          j++;
+          k--;
+        } else if (sum < target) {
+          j++;
+        } else {
+          k--;
+        }
+      }
+    }
+  }
+
+  public static int[] findProduct(int[] arr) {
+    int temp = 1;
+    int[] result = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      result[i] = temp;
+      temp = temp * arr[i];
+    }
+    temp = 1;
+    for (int i = arr.length - 1; i >= 0; i--) {
+      result[i] = result[i] * temp;
+      temp = temp * arr[i];
+    }
+    return result;
   }
 
 
